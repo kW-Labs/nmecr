e89a91437f1a7b112b4b83f4a18b69629f52c2a6
MeeraSharma <mrinalini04@gmail.com>
2019-09-20 11:14:54 -0700

    Updated names of package datasets

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' Temperature data.
+#'
+#' A dataset containing the daily average temperature for the time period: 04/07/2017 - 04/07/2019.
+#'
+#'
+#' @format A data frame with 732 rows and 2 variables:
+#' \describe{
+#'   \item{time}{timestamp, format mm/dd/yy}
+#'   \item{temp}{outside air temperature corresponding to the timestamp, in deg F}
+#'   ...
+#' }
+"temp"
+
+
+#' Energy Use data.
+#'
+#' A dataset containing the daily average temperature for the time period: 04/07/2017 - 04/07/2019.
+#'
+#'
+#' @format A data frame with 732 rows and 2 variables:
+#' \describe{
+#'   \item{time}{timestamp, format mm/dd/yy}
+#'   \item{eload}{energy consumption data aggregated to the daily interval, in kWh}
+#'   ...
+#' }
+"eload"
