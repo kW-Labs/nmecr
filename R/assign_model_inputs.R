e27707ae70b348a198174080b1d728dc2bec7df4
MeeraSharma <mrinalini04@gmail.com>
2019-10-31 16:36:59 -0700

    metadata file for model inputs

---
+++ R/assign_model_inputs.R
@@ -0,0 +1,29 @@
+
+
+
+
+
+assign_model_inputs <- function(timescale_days = NULL,
+                             interval_minutes = 15,
+                             has_temp_knots_defined = FALSE,
+                             equal_temp_segment_points = TRUE,
+                             temp_segments_numeric = 6,
+                             temp_knots_value = c(40, 45, 50, 60, 65, 90),
+                             regression_type = c("TOWT", "Time-only", "SLR", "HDD-CDD Multivariate Regression", "HDD Regression", "CDD Regression",
+                                                 "Three Parameter Cooling", "Three Parameter Heating", "Five Parameter Linear Model",
+                                                 "Five Parameter Linear Model")){
+
+  out <- list()
+
+  out$timescale_days = timescale_days
+  out$interval_minutes = interval_minutes
+  out$has_temp_knots_defined = has_temp_knots_defined
+  out$equal_temp_segment_points = equal_temp_segment_points
+  out$temp_segments_numeric = temp_segments_numeric
+  out$temp_knots_value = temp_knots_value
+  out$regression_type = match.arg(regression_type)
+
+  return(out)
+
+
+}
