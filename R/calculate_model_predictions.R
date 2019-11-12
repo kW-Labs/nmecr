5d4163c95db6667a90f971636b4971804244fc2f
MeeraSharma <mrinalini04@gmail.com>
2019-11-12 15:41:50 -0800

    generalized model predictions

--- R/calculate_model_predictions.R
+++ R/calculate_model_predictions.R
@@ -2,27 +2,39 @@
 
 calculate_model_predictions <- function(prediction_list = NULL, modeled_object = NULL, model_input_options = NULL) {
 
-  if(model_input_options$regression_type == "SLR") {
+  dframe_pred <- prediction_list$dataframe
 
-    dframe_pred <- prediction_list$dataframe
+  if(model_input_options$regression_type == "TOWT" |
+     model_input_options$regression_type == "Time-only" |
+     model_input_options$regression_type == "SLR" |
+     model_input_options$regression_type == "HDD-CDD Multivariate Regression" |
+     model_input_options$regression_type == "HDD Regression" |
+     model_input_options$regression_type == "CDD Regression") {
 
     if(! is.null(prediction_list$operating_mode_data)){
       dframe_pred <- dplyr::inner_join(dframe_pred, prediction_list$operating_mode_data, by = "time")
     }
 
-    dframe_pred <- dframe_pred %>%
-      dplyr::select(-"time")
+    # if column names are different, throw an error
+
+    predictions <- predict(modeled_object$model, dframe_pred)
+
+    out <- NULL
+    out$predictions <- data.frame(prediction_list$dataframe, predictions)
 
   } else if (model_input_options$regression_type == "Three Parameter Cooling" | model_input_options$regression_type == "Three Parameter Heating" |
              model_input_options$regression_type == "Four Parameter Linear Model" | model_input_options$regression_type == "Five Parameter Linear Model") {
 
-    dframe_pred <- prediction_list$dataframe$temp
+    dframe_pred <- data.frame(independent_variable = prediction_list$dataframe$temp)
+
+    predictions <- segmented::predict.segmented(object = modeled_object$model, newdata = dframe_pred)
+
+    out <- NULL
+    out$predictions <- data.frame(prediction_list$dataframe, predictions)
 
   }
 
-  predictions <- predict(modeled_object$model, newdata = dframe_pred)
-
-  out <- NULL
-  out$predictions <- data.frame(prediction_list$dataframe, predictions)
+  return(out)
 
 }
+
