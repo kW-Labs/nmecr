e48565ef399e1541e9ea167744b6b4e0b505f848
MeeraSharma <mrinalini04@gmail.com>
2019-11-12 18:27:51 -0800

    added predictions for TOWT and Time-only

--- R/calculate_model_predictions.R
+++ R/calculate_model_predictions.R
@@ -1,12 +1,10 @@
 
 
-calculate_model_predictions <- function(prediction_list = NULL, modeled_object = NULL, model_input_options = NULL) {
+calculate_model_predictions <- function(training_list = NULL, prediction_list = NULL, modeled_object = NULL, model_input_options = NULL) {
 
   dframe_pred <- prediction_list$dataframe
 
-  if(model_input_options$regression_type == "TOWT" |
-     model_input_options$regression_type == "Time-only" |
-     model_input_options$regression_type == "SLR" |
+  if(model_input_options$regression_type == "SLR" |
      model_input_options$regression_type == "HDD-CDD Multivariate Regression" |
      model_input_options$regression_type == "HDD Regression" |
      model_input_options$regression_type == "CDD Regression") {
@@ -15,9 +13,20 @@ calculate_model_predictions <- function(prediction_list = NULL, modeled_object =
       dframe_pred <- dplyr::inner_join(dframe_pred, prediction_list$operating_mode_data, by = "time")
     }
 
-    # if column names are different, throw an error
-
     predictions <- predict(modeled_object$model, dframe_pred)
+
+    out <- NULL
+    out$predictions <- data.frame(prediction_list$dataframe, predictions)
+
+  } else if(model_input_options$regression_type == "TOWT" |
+            model_input_options$regression_type == "Time-only") {
+
+    if(! is.null(prediction_list$operating_mode_data)){
+      dframe_pred <- dplyr::inner_join(dframe_pred, prediction_list$operating_mode_data, by = "time")
+    }
+
+    predictions <- calculate_TOWT_model_predictions(training_list = training_list, prediction_list = prediction_list,
+                                                    modeled_object = modeled_object, model_input_options = model_input_options)
 
     out <- NULL
     out$predictions <- data.frame(prediction_list$dataframe, predictions)
