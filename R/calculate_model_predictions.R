ac6ffa8e469d5b0b4b49b0bfdcaae3e1e5c72cbb
MeeraSharma <mrinalini04@gmail.com>
2019-11-12 18:58:53 -0800

    using information from model output for model specifications

--- R/calculate_model_predictions.R
+++ R/calculate_model_predictions.R
@@ -1,13 +1,13 @@
 
 
-calculate_model_predictions <- function(training_list = NULL, prediction_list = NULL, modeled_object = NULL, model_input_options = NULL) {
+calculate_model_predictions <- function(training_list = NULL, prediction_list = NULL, modeled_object = NULL) {
 
   dframe_pred <- prediction_list$dataframe
 
-  if(model_input_options$regression_type == "SLR" |
-     model_input_options$regression_type == "HDD-CDD Multivariate Regression" |
-     model_input_options$regression_type == "HDD Regression" |
-     model_input_options$regression_type == "CDD Regression") {
+  if(modeled_object$model_input_options$regression_type == "SLR" |
+     modeled_object$model_input_options$regression_type == "HDD-CDD Multivariate Regression" |
+     modeled_object$model_input_options$regression_type == "HDD Regression" |
+     modeled_object$model_input_options$regression_type == "CDD Regression") {
 
     if(! is.null(prediction_list$operating_mode_data)){
       dframe_pred <- dplyr::inner_join(dframe_pred, prediction_list$operating_mode_data, by = "time")
@@ -18,21 +18,21 @@ calculate_model_predictions <- function(training_list = NULL, prediction_list = 
     out <- NULL
     out$predictions <- data.frame(prediction_list$dataframe, predictions)
 
-  } else if(model_input_options$regression_type == "TOWT" |
-            model_input_options$regression_type == "Time-only") {
+  } else if(modeled_object$model_input_options$regression_type == "TOWT" |
+            modeled_object$model_input_options$regression_type == "Time-only") {
 
     if(! is.null(prediction_list$operating_mode_data)){
       dframe_pred <- dplyr::inner_join(dframe_pred, prediction_list$operating_mode_data, by = "time")
     }
 
     predictions <- calculate_TOWT_model_predictions(training_list = training_list, prediction_list = prediction_list,
-                                                    modeled_object = modeled_object, model_input_options = model_input_options)
+                                                    modeled_object = modeled_object, modeled_object$model_input_options = modeled_object$model_input_options)
 
     out <- NULL
     out$predictions <- data.frame(prediction_list$dataframe, predictions)
 
-  } else if (model_input_options$regression_type == "Three Parameter Cooling" | model_input_options$regression_type == "Three Parameter Heating" |
-             model_input_options$regression_type == "Four Parameter Linear Model" | model_input_options$regression_type == "Five Parameter Linear Model") {
+  } else if (modeled_object$model_input_options$regression_type == "Three Parameter Cooling" | modeled_object$model_input_options$regression_type == "Three Parameter Heating" |
+             modeled_object$model_input_options$regression_type == "Four Parameter Linear Model" | modeled_object$model_input_options$regression_type == "Five Parameter Linear Model") {
 
     dframe_pred <- data.frame(independent_variable = prediction_list$dataframe$temp)
 
