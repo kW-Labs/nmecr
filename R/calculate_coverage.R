58617de670ef83b867a09bdf2a167b2b1f093cf4
MeeraSharma <mrinalini04@gmail.com>
2019-11-14 12:37:19 -0800

    coverage factor refactoring complete

--- R/calculate_coverage.R
+++ R/calculate_coverage.R
@@ -25,6 +25,10 @@
 calculate_coverage <- function(dataframe = NULL, ref_temp_data = NULL,
                                outlier_threshold = NULL, extrapolation_limit = NULL) {
 
+  if(dataframe$chosen_modeling_interval == "Monthly") {
+    stop("Please upload a dataframe in hourlt or daily time data intervals")
+  }
+
   if(! assertive::is_numeric(outlier_threshold)){
     stop("Error: outlier_threshold must be a numeric input")
   }
@@ -42,10 +46,6 @@ calculate_coverage <- function(dataframe = NULL, ref_temp_data = NULL,
     ref_temp_interval <- "Daily"
   } else {
     stop("Please upload ref_temp_data in hourly or daily time data intervals")
-  }
-
-  if(dataframe$chosen_modeling_interval == "Monthly") {
-    stop("Please upload a dataframe in hourlt or daily time data intervals")
   }
 
   site_temp_data <- dataframe$dataframe[, c("time", "temp")]
@@ -69,7 +69,7 @@ calculate_coverage <- function(dataframe = NULL, ref_temp_data = NULL,
   mround <- function(number, multiple=2) {
     n <- number / multiple
     if (abs(n - trunc(n)) == 0.5) {
-      n <- n - 0.1
+      n <- n + 0.1
     } else {
       n <- n
     }
@@ -78,60 +78,52 @@ calculate_coverage <- function(dataframe = NULL, ref_temp_data = NULL,
 
   # Creating an empty temperature bin matrix
   bins <- seq(0, 120, by = 2)
-
   temp_data <- as.data.frame(bins)
+  temp_data$bins <- as.character(temp_data$bins)
   colnames(temp_data) <- c("bins")
 
   # Binning site temperature data
 
-  site_temp_bins <- sapply(site_temp_data$temp, mround) %>%
-    as.data.frame()
+  site_temp_data <- site_temp_data %>%
+    mutate("bins" = sapply(temp, mround))
 
-  colnames(site_temp_bins) <- c("bins")
-
+  site_temp_bins <- site_temp_data$bins
   count_site_bins <- as.data.frame(table(site_temp_bins))
   colnames(count_site_bins) <- c("bins", "frequency")
-  count_site_bins$bins <- as.numeric(count_site_bins$bins)
+  count_site_bins$bins <-as.character(count_site_bins$bins)
 
-  # Populating the temperature bin matrix created above
+  # Binning reference temperature data
+
+  ref_temp_data <- ref_temp_data %>%
+    mutate("bins" = sapply(temp, mround))
+
+  ref_data_bins <- ref_temp_data$bins
+  count_ref_data_bins <- as.data.frame(table(ref_data_bins))
+  colnames(count_ref_data_bins) <- c("bins", "frequency")
+  count_ref_data_bins$bins <-as.character(count_ref_data_bins$bins)
+
+  # Populating the temperature bin
 
   temp_coverage <- left_join(temp_data, count_site_bins, by = "bins")
   colnames(temp_coverage) <- c("bins", "site_data")
   temp_coverage$site_data[is.na(temp_coverage$site_data)] <- 0
 
-  # Binning reference temperature data
-
-  ref_data_bins <- sapply(ref_temp_data[[2]], mround)
-  ref_data_bins <- as_data_frame(ref_data_bins)
-  colnames(ref_data_bins) <- c("bins")
-
-  count_ref_data_bins <- as.data.frame(table(ref_data_bins))
-  colnames(count_ref_data_bins) <- c("bins", "frequency")
-
-  # Populating the temperature bin matrix created above
-
   temp_coverage <- left_join(temp_coverage, count_ref_data_bins, by = "bins")
   colnames(temp_coverage) <- c("bins", "n_site_data", "n_ref_data")
   temp_coverage$n_ref_data[is.na(temp_coverage$n_ref_data)] <- 0
-
-  temp_coverage$bins <- as.numeric(temp_coverage$bins)
 
   out <- NULL
   out$temp_coverage <- temp_coverage
 
   # Calculating Coverage Factor
 
-  outlier_threshold <- outlier_threshold
-  max_obs_OA_bin <- max(as.numeric(temp_coverage$bins[temp_coverage$n_site_data > outlier_threshold |
-                                                        temp_coverage$n_site_data == outlier_threshold]))
-  min_obs_OA_bin <- min(as.numeric(temp_coverage$bins[temp_coverage$n_site_data > outlier_threshold |
-                                                        temp_coverage$n_site_data == outlier_threshold]))
-
-  obs_weather_range <- max_obs_OA_bin - min_obs_OA_bin
-  extrapolation_limit <- extrapolation_limit
+  max_obs_OA_bin <- max(as.numeric(temp_coverage$bins[temp_coverage$n_site_data >= outlier_threshold]))
+  min_obs_OA_bin <- min(as.numeric(temp_coverage$bins[temp_coverage$n_site_data > outlier_threshold]))
 
   max_ref_OA_bin <- max(as.numeric(temp_coverage$bins[temp_coverage$n_ref_data > 0.1]))
   min_ref_OA_bin <- min(as.numeric(temp_coverage$bins[temp_coverage$n_ref_data > 0.1]))
+
+  obs_weather_range <- max_obs_OA_bin - min_obs_OA_bin
 
   extrapolated_max_obs_OA_bin <- mround(max_obs_OA_bin + (extrapolation_limit * obs_weather_range),2)
   extrapolated_min_obs_OA_bin <- mround(min_obs_OA_bin - (extrapolation_limit * obs_weather_range),2)
@@ -139,41 +131,7 @@ calculate_coverage <- function(dataframe = NULL, ref_temp_data = NULL,
   temp_coverage_factor <- signif(min(100, (100 * (extrapolated_max_obs_OA_bin - extrapolated_min_obs_OA_bin) /
                                              (max_ref_OA_bin - min_ref_OA_bin))), 4)
 
-  if (data_interval == "Hourly") {
-
-    peak_hours <- max(temp_coverage$n_site_data)
-    peak_bin_hourly <- as.numeric(temp_coverage$bins[which(temp_coverage$n_site_data == max(temp_coverage$n_site_data))])
-
-    hours_covered <- temp_coverage %>%
-      filter(bins <= extrapolated_max_obs_OA_bin & bins >= extrapolated_min_obs_OA_bin)
-
-    hours_covered <- sum(hours_covered$n_ref_data)
-
-    hours_not_covered <- 8760 - hours_covered
-    hourly_coverage_factor <- signif((hours_covered / 8760) * 100, 4)
-
-    coverage_factor_summary <- as.data.frame(matrix(nr = 6, nc = 2))
-    names(coverage_factor_summary) <- c("Coverage Factor Summary", "")
-
-    coverage_factor_summary[1, 1] <- "Temperature Coverage"
-    coverage_factor_summary[1, 2] <- paste(temp_coverage_factor, "%", sep = "")
-
-    coverage_factor_summary[2, 1] <- "Time Coverage (Hours)"
-    coverage_factor_summary[2, 2] <- paste(hourly_coverage_factor, "%", sep = "")
-
-    coverage_factor_summary[3, 1] <- "Hours Covered"
-    coverage_factor_summary[3, 2] <- format(hours_covered, scientific = FALSE, big.mark = ",", digits = 2)
-
-    coverage_factor_summary[4, 1] <- "Hours Not Covered"
-    coverage_factor_summary[4, 2] <- format(hours_not_covered, scientific = FALSE, big.mark = ",", digits = 2)
-
-    coverage_factor_summary[5, 1] <- "Peak Temperature Bin"
-    coverage_factor_summary[5, 2] <- format(peak_bin_hourly, scientific = FALSE, big.mark = ",", digits = 2)
-
-    coverage_factor_summary[6, 1] <- "Number of hours in Peak Temperature Bin"
-    coverage_factor_summary[6, 2] <- format(peak_hours, scientific = FALSE, big.mark = ",", digits = 2)
-
-  } else if (data_interval == "Daily") {
+  if (dataframe$chosen_modeling_interval == "Daily") {
 
     peak_days <- max(temp_coverage$n_site_data)
     peak_bin_daily <- as.numeric(temp_coverage$bins[which(temp_coverage$n_site_data == max(temp_coverage$n_site_data))])
@@ -186,26 +144,60 @@ calculate_coverage <- function(dataframe = NULL, ref_temp_data = NULL,
     days_not_covered <- 365 - days_covered
     daily_coverage_factor <- signif((days_covered / 365) * 100, 4)
 
-    coverage_factor_summary <- as.data.frame(matrix(nr = 6, nc = 2))
-    names(coverage_factor_summary) <- c("Coverage Factor Summary", "")
+  } else {
 
-    coverage_factor_summary[1, 1] <- "Temperature Coverage"
-    coverage_factor_summary[1, 2] <- paste(temp_coverage_factor, "%", sep = "")
+    peak_hours <- max(temp_coverage$n_site_data)
+    peak_bin_hourly <- as.numeric(temp_coverage$bins[which(temp_coverage$n_site_data == max(temp_coverage$n_site_data))])
 
-    coverage_factor_summary[2, 1] <- "Time Coverage (Days)"
-    coverage_factor_summary[2, 2] <- paste(daily_coverage_factor, "%", sep = "")
+    hours_covered <- temp_coverage %>%
+      filter(bins <= extrapolated_max_obs_OA_bin & bins >= extrapolated_min_obs_OA_bin)
+
+    hours_covered <- sum(hours_covered$n_ref_data)
+
+    hours_not_covered <- 8760 - hours_covered
+    hourly_coverage_factor <- signif((hours_covered / 8760) * 100, 4)
+
+  }
+
+  coverage_factor_summary <- as.data.frame(matrix(nr = 6, nc = 2))
+
+  coverage_factor_summary[1, 1] <- "Temperature Coverage"
+  coverage_factor_summary[1, 2] <- temp_coverage_factor
+
+
+  if (dataframe$chosen_modeling_interval == "Daily") {
+
+    coverage_factor_summary[2, 1] <- "Time Coverage"
+    coverage_factor_summary[2, 2] <- daily_coverage_factor
 
     coverage_factor_summary[3, 1] <- "Days Covered"
-    coverage_factor_summary[3, 2] <- format(days_covered, scientific = FALSE, big.mark = ",", digits = 2)
+    coverage_factor_summary[3, 2] <- days_covered
 
     coverage_factor_summary[4, 1] <- "Days Not Covered"
-    coverage_factor_summary[4, 2] <- format(days_not_covered, scientific = FALSE, big.mark = ",", digits = 2)
+    coverage_factor_summary[4, 2] <- days_not_covered
 
     coverage_factor_summary[5, 1] <- "Peak Temperature Bin"
-    coverage_factor_summary[5, 2] <- format(peak_bin_daily, scientific = FALSE, big.mark = ",", digits = 2)
+    coverage_factor_summary[5, 2] <- peak_bin_daily
 
     coverage_factor_summary[6, 1] <- "Number of days in Peak Temperature Bin"
-    coverage_factor_summary[6, 2] <- format(peak_days, scientific = FALSE, big.mark = ",", digits = 2)
+    coverage_factor_summary[6, 2] <- peak_days
+
+  } else {
+
+    coverage_factor_summary[2, 1] <- "Time Coverage"
+    coverage_factor_summary[2, 2] <- hourly_coverage_factor
+
+    coverage_factor_summary[3, 1] <- "Hours Covered"
+    coverage_factor_summary[3, 2] <- hours_covered
+
+    coverage_factor_summary[4, 1] <- "Hours Not Covered"
+    coverage_factor_summary[4, 2] <- hours_not_covered
+
+    coverage_factor_summary[5, 1] <- "Peak Temperature Bin"
+    coverage_factor_summary[5, 2] <- peak_bin_hourly
+
+    coverage_factor_summary[6, 1] <- "Number of hours in Peak Temperature Bin"
+    coverage_factor_summary[6, 2] <- peak_hours
 
   }
 
