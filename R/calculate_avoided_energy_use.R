5231457a2afacbc25f652ad71ac07f1bf812e38e
MeeraSharma <mrinalini04@gmail.com>
2019-11-13 16:44:51 -0800

    refactored savings and uncertainty analysis

--- R/calculate_avoided_energy_use.R
+++ R/calculate_avoided_energy_use.R
@@ -12,31 +12,88 @@
 #' }
 #' @export
 
-calculate_avoided_energy_use <- function(modeled_data_obj){
-  pred <- modeled_data_obj$post_implementation_data
-  # actual eload during prediction period = post-measure period
-  act_post <- pred$eload
-  # prediction of eload during prediction period = post period
-  pred_post <- pred$pred_eload
-  sum_pred_post <- sum(pred_post, na.rm = TRUE)
+calculate_savings_and_uncertainty <- function(prediction_results_list = NULL, modeled_object = NULL, model_summary_statistics = NULL, confidence_level = 90){
 
-  # savings:
-  savings <- sum((pred_post - act_post), na.rm = TRUE)
-  savings_df <- as.data.frame(pred_post - act_post)
-  names(savings_df) <- "savings"
-  savings_df <- dplyr::bind_cols("time" = pred$time, savings_df)
-  pct_savings <- (savings / sum_pred_post)
-  baseline_energy_use <- sum(modeled_data_obj$training_data$eload, na.rm = T)
+  correlation_df <- as.data.frame(matrix(nrow = length(modeled_object$training_data$eload), ncol = 2))
+  names(correlation_df) <- c("residuals", "residuals_shifted")
+
+  correlation_df$residuals <- modeled_object$training_data$eload - modeled_object$training_data$model_fit
+  correlation_df$residuals_shifted <- dplyr::lag(correlation_df$residuals, 1)
+  correlation_df <- correlation_df[-1, ]
+
+  rho <- cor(correlation_df[,1], correlation_df[,2])
+  n <- length(modeled_object$training_data$time)
+  n_dash <- n*rho
 
   results <- NULL
-  savings_summary <- as.data.frame(matrix(nr = 1, nc = 2))
-  names(savings_summary) <- c("Savings", "Savings (%)")
 
-  savings_summary$'Savings' <- formatC(savings, format = "d")
-  savings_summary$'Savings (%)' <- paste(round(pct_savings * 100, 2), "%", sep = "")
+  if(! is.null(prediction_results_list)) {
 
-  results$savings_summary <-  savings_summary
-  results$'savings_df' <- savings_df
-  results$pct_savings <- round(pct_savings * 100, 2)
+    savings_df <- prediction_results_list$predictions[, c("time", "eload", "predictions")] %>%
+      mutate(savings = predictions - eload)
+
+    savings_summary_df <- as.data.frame(matrix(nrow = 1, ncol = 4))
+    names(savings_summary_df) <- c("performance_period_use", "adjusted_baseline_use", "savings", "savings_pct")
+    savings_summary_df$performance_period_use <- sum(savings_df$eload, na.rm = T)
+    savings_summary_df$adjusted_baseline_use <- sum(savings_df$predictions, na.rm = T)
+    savings_summary_df$savings <- sum(savings_df$savings, na.rm = T)
+    savings_summary_df$savings_pct <- round(savings_summary_df$savings/savings_summary_df$adjusted_baseline_use, 2)
+
+    m <- length(prediction_results_list$predictions$time)
+
+    results$savings_df <-  savings_df
+
+  } else {
+
+    m <- n
+    savings_summary_df <- as.data.frame(matrix(nrow = 1, ncol = 1))
+    names(savings_summary_df) <- "savings_pct"
+    savings_summary_df$savings_pct <- 0.1
+
+  }
+
+  # savings uncertainty ----
+
+  uncertainty_50 <- 0.5
+
+  t_stat <- stats::qt(1 - (1 - (confidence_level/100)) / 2, 100000)
+
+  if (modeled_object$model_input_options$chosen_modeling_interval == "Hourly") {
+
+    alpha <- 1.26
+
+  } else if (modeled_object$model_input_options$chosen_modeling_interval == "Daily") {
+
+    observation_count <- length(modeled_object$training_data$time) %>%
+      magrittr::divide_by(mean(30, 31))
+
+    alpha <- ( - 0.00024 * (observation_count ^ 2) + (0.03535 * (observation_count) + 1.00286))
+
+  } else if (modeled_object$model_input_options$chosen_modeling_interval == "Monthly") {
+
+    observation_count <- length(modeled_object$training_data$time)
+
+    alpha <- (- 0.00022 * (observation_count ^ 2)) + (0.03306 * (observation_count)) + 0.94054
+  }
+
+  if (modeled_object$model_input_options$chosen_modeling_interval == "Monthly") {
+
+    savings_uncertainty <- t_stat * alpha * model_summary_statistics$CVRMSE * sqrt((1 + (2 / n)) *  (1 / m)) / savings_summary_df$savings_pct
+    savings_pct_for_50pct_uncertainty <-  t_stat * alpha * model_summary_statistics$CVRMSE * sqrt((1 + 2 / n) * (1 / m)) / uncertainty_50
+
+  } else {
+
+    savings_uncertainty <-  t_stat * alpha * model_summary_statistics$CVRMSE * sqrt(((n / n_dash) * (1 + (2 / n_dash)) *  (1 / m))) / savings_summary_df$savings_pct
+    savings_pct_for_50pct_uncertainty <-  t_stat * alpha * model_summary_statistics$CVRMSE * sqrt(((n / n_dash) * (1 + (2 / n_dash)) * (1 / m))) / uncertainty_50
+
+  }
+
+  savings_summary_df <- savings_summary_df %>%
+    mutate(savings_uncertainty = savings_uncertainty) %>%
+    mutate(savings_pct_for_50pct_uncertainty = savings_pct_for_50pct_uncertainty) %>%
+    mutate(confidence_level = confidence_level)
+
+  results$savings_summary_df <- savings_summary_df
+
   return(results)
 }
