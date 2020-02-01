882071584653e68f3c040edda42dd600862593d4
MeeraSharma <mrinalini04@gmail.com>
2020-02-01 13:55:46 -0800

    summary stats updated to reflect ASHRAE requirements

--- R/calculate_summary_statistics.R
+++ R/calculate_summary_statistics.R
@@ -46,14 +46,16 @@ calculate_summary_statistics <- function(modeled_data_obj = NULL) {
     magrittr::raise_to_power(0.5)
 
 
-  # Coefficient of Variation of the Root Mean Square Error (Absolute)
+  # Coefficient of Variation of the Root Mean Square Error (Percentage)
   CVRMSE <- RMSE %>%
-    magrittr::divide_by(mean(eload, na.rm = T))
+    magrittr::divide_by(mean(eload, na.rm = T)) %>%
+    magrittr::multiply_by(100)
 
-  # Mean Bias Error (Absolute)
-  MBE <- fit_residuals_numeric %>%
+  # Normalized Mean Bias Error (Percentage)
+  NMBE <- fit_residuals_numeric %>%
     sum(na.rm = T) %>%
-    magrittr::divide_by(length(fit_residuals_numeric))
+    magrittr::divide_by(effective_parameters*mean(eload, na.rm = T)) %>%
+    magrittr::multiply_by(100)
 
   # Coefficient of Variation of Mean Absolute error (Absolute)
   CVMAE <- fit_residuals_numeric %>%
@@ -62,18 +64,19 @@ calculate_summary_statistics <- function(modeled_data_obj = NULL) {
     sum(na.rm = T) %>%
     magrittr::divide_by(mean(eload, na.rm = T))
 
-  # Net Determination Bias Error (Absolute)
+  # Net Determination Bias Error (Percentage)
 
   NDBE <- fit_residuals_numeric %>%
     sum(na.rm = T) %>%
-    magrittr::divide_by(sum(eload, na.rm = T))
+    magrittr::divide_by(sum(eload, na.rm = T)) %>%
+    magrittr::multiply_by(100)
 
   goodness_of_fit <- as.data.frame(matrix(nr = 1, nc = 5))
-  names(goodness_of_fit) <- c("R_squared", "CVRMSE", "NDBE", "MBE", "#Parameters")
+  names(goodness_of_fit) <- c("R_squared", "CVRMSE %", "NDBE %", "NMBE %", "#Parameters")
   goodness_of_fit$R_squared <- R_squared
-  goodness_of_fit$CVRMSE <- CVRMSE
-  goodness_of_fit$NDBE <- NDBE
-  goodness_of_fit$MBE <- MBE
+  goodness_of_fit$`CVRMSE %` <- CVRMSE
+  goodness_of_fit$`NDBE %` <- NDBE
+  goodness_of_fit$`NMBE %` <- NMBE
   goodness_of_fit$"#Parameters" <- nparameter
 
   return(goodness_of_fit)
