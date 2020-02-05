c364c74231b1eaee13251ad4e414199af97c5b66
MeeraSharma <mrinalini04@gmail.com>
2020-02-05 14:18:31 -0800

    Monthly Analysis based on bill read dates

--- R/aggregate.R
+++ R/aggregate.R
@@ -92,35 +92,95 @@ aggregate <- function(eload_data = NULL, temp_data = NULL, convert_to_data_inter
     CDD <- daily_temp$temp - base_temp
     CDD[CDD < 0 ] <- 0
 
-
     daily_data <- dplyr::bind_cols(daily_temp, "HDD" = HDD, "CDD" = CDD)
 
-    monthly_temp <- daily_data %>%
-      dplyr::mutate(month = lubridate::floor_date(daily_data$time, "month")) %>%
-      dplyr::group_by("time" = month) %>%
-      dplyr::summarize("temp" = mean(temp, na.rm = T),
-                "HDD" = sum(HDD, na.rm = T),
-                "CDD" = sum(CDD, na.rm = T))
-
-    monthly_temp <- monthly_temp %>%
-      dplyr::mutate(days = lubridate::days_in_month(monthly_temp$time)) %>%
-      dplyr::mutate(HDDperday = HDD / days) %>%
-      dplyr::mutate(CDDperday = CDD / days) %>%
-      na.omit()
-
-    if(! is.null(eload_data)) {
-
-      monthly_eload <- eload_data %>%
-        dplyr::mutate(month = lubridate::floor_date(eload_data$time, "month")) %>%
+    if (is.null (eload_data)){
+      monthly_temp <- daily_data %>%
+        dplyr::mutate(month = lubridate::floor_date(daily_data$time, "month")) %>%
         dplyr::group_by("time" = month) %>%
-        dplyr::summarize("eload" = sum(eload, na.rm = T)) %>%
+        dplyr::summarize("temp" = mean(temp, na.rm = T),
+                         "HDD" = sum(HDD, na.rm = T),
+                         "CDD" = sum(CDD, na.rm = T))
+
+      monthly_temp <- monthly_temp %>%
+        dplyr::mutate(days = lubridate::days_in_month(monthly_temp$time)) %>%
+        dplyr::mutate(HDDperday = HDD / days) %>%
+        dplyr::mutate(CDDperday = CDD / days) %>%
         na.omit()
 
-      aggregated_data <- monthly_temp %>%
-        dplyr::inner_join(monthly_eload, by = "time") %>%
-        dplyr::mutate(eloadperday = eload/days) %>%
-        dplyr::distinct()
+    } else if (! is.null(eload_data)) {
 
+      nterval_eload <- median(diff(as.numeric(eload_data$time)))/60
+
+      if(nterval_eload < 40320) {
+
+        monthly_temp <- daily_data %>%
+          dplyr::mutate(month = lubridate::floor_date(daily_data$time, "month")) %>%
+          dplyr::group_by("time" = month) %>%
+          dplyr::summarize("temp" = mean(temp, na.rm = T),
+                    "HDD" = sum(HDD, na.rm = T),
+                    "CDD" = sum(CDD, na.rm = T))
+
+        monthly_temp <- monthly_temp %>%
+          dplyr::mutate(days = lubridate::days_in_month(monthly_temp$time)) %>%
+          dplyr::mutate(HDDperday = HDD / days) %>%
+          dplyr::mutate(CDDperday = CDD / days) %>%
+          na.omit()
+
+      } else {
+
+        monthly_temp <- data.frame("time" = eload_data$time)
+
+        monthly_temp <- data.frame(matrix(ncol = 5, nrow = length(eload_data$time)))
+        names(monthly_temp) <- c("time", "temp", "HDD", "CDD", "days")
+        monthly_temp$time <- eload_data$time
+
+        for (i in 2:length(eload_data$time)) {
+          monthly_temp$temp[i] <- daily_data %>%
+            dplyr::filter(time >= monthly_temp$time[i] & time < monthly_temp$time[i+1]) %>%
+            dplyr::summarize("temp" = round(mean(temp),2)) %>%
+            as.numeric()
+
+          monthly_temp$HDD[i] <- daily_data %>%
+            dplyr::filter(time >= monthly_temp$time[i] & time < monthly_temp$time[i+1]) %>%
+            dplyr::summarize("HDD" = round(sum(HDD),2)) %>%
+            as.numeric
+
+          monthly_temp$CDD[i] <- daily_data %>%
+            dplyr::filter(time >= monthly_temp$time[i] & time < monthly_temp$time[i+1]) %>%
+            dplyr::summarize("CDD" = round(sum(CDD),2)) %>%
+            as.numeric()
+
+          monthly_temp$days[i] <- difftime(monthly_temp$time[i+1], monthly_temp$time[i], units = "days") %>%
+            as.numeric()
+        }
+
+        monthly_temp <- monthly_temp[complete.cases(monthly_temp), ] %>%
+          dplyr::mutate(HDDperday = HDD / days) %>%
+          dplyr::mutate(CDDperday = CDD / days)
+      }
+    }
+      if(! is.null(eload_data)) {
+
+        nterval_eload <- median(diff(as.numeric(eload_data$time)))/60
+
+        if(nterval_eload < 40320) { # using 28 days
+
+          monthly_eload <- eload_data %>%
+            dplyr::mutate(month = lubridate::floor_date(eload_data$time, "month")) %>%
+            dplyr::group_by("time" = month) %>%
+            dplyr::summarize("eload" = sum(eload, na.rm = T)) %>%
+            na.omit()
+
+        } else {
+
+          monthly_eload <- eload_data
+        }
+
+        aggregated_data <- monthly_temp %>%
+          dplyr::inner_join(monthly_eload, by = "time") %>%
+          dplyr::mutate(eloadperday = eload/days) %>%
+          dplyr::distinct()
 
       return(aggregated_data)
 
