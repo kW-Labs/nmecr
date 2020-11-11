69073f8db7633cfe788a9d44ddfc04111101f84a
Mrinalini Sharma <mrinalini04@gmail.com>
2020-11-10 19:37:26 -0800

    align_data() - precrusor to create_dataframe()

---
+++ R/align_data.R
@@ -0,0 +1,54 @@
+#' Align a timeseries to the start of a period.
+#'
+#' \code{This function serves as a precursor to create_dataframe().}
+#'
+#' @param data A dataframe with a timestamp and a variable. Column names: "time" and "data".
+#' @param original_alignment A string indicating the original alignment of the input data. Options: 'start', 'end'.
+#'
+#' @return a dataframe with data aligned to the start of the period
+#' @export
+
+
+align_data <- function(data = NULL, original_alignment = NULL) {
+
+  if(is.null(data)) {
+    stop("Please input data.")
+  }
+
+  if(is.null(original_alignment)) {
+    stop("Please specify the original alignment of the data. Options: 'start', 'end'.")
+  }
+
+  if (original_alignment == 'start'){
+    aligned_data <- data
+  } else if (original_alignment == 'end') {
+
+    data <- as.data.frame(data)
+    data_xts <- xts::xts(x = data[, -1], order.by = data[, 1])
+    data_scale <- xts::periodicity(data_xts)['scale']
+
+   if (data_scale == "monthly"){
+      correction <- 60*60*24*mean(30,31)
+      corrected_index <- lubridate::floor_date(zoo::index(data_xts) - correction, "month")
+    } else if(data_scale == "minute"){
+      correction <- 60*15
+      corrected_index <- zoo::index(data_xts) - correction
+    } else if (data_scale == "hourly"){
+      correction <- 60*60
+      corrected_index <- zoo::index(data_xts) - correction
+    } else if (data_scale == "daily"){
+      correction <- 60*60*24
+      corrected_index <- zoo::index(data_xts) - correction
+    }
+
+    data_xts <- xts::xts(data_xts, corrected_index)
+
+    aligned_data <- timetk::tk_tbl(data = data_xts, preserve_index = TRUE, rename_index = "time")
+
+  } else {
+    stop("original_alignment can one of two: 'start' or 'end'. Please check your input")
+  }
+
+  return(aligned_data)
+
+}
