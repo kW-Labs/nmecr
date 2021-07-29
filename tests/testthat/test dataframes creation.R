adcc2926c51f6510aa15d16ca9c54d67adcd89db
Mrinalini Sharma <mrinalini04@gmail.com>
2021-07-29 16:16:11 -0700

    dataset update to continuous additional variable data

--- tests/testthat/test dataframes creation.R
+++ tests/testthat/test dataframes creation.R
@@ -7,6 +7,12 @@ simple_df_daily <- readRDS("Expectations/simple_df_daily.rds")
 simple_df_monthly <- readRDS("Expectations/simple_df_monthly.rds")
 
 
+added_variable_df <- readRDS("Expectations/added_variable_df.rds")
+added_variable_df_hourly <- readRDS("Expectations/added_variable_df_hourly.rds")
+added_variable_df_daily <- readRDS("Expectations/added_variable_df_daily.rds")
+added_variable_df_monthly <- readRDS("Expectations/added_variable_df_monthly.rds")
+
+
 # Data and Dataframes
 
 eload_data <- readRDS("Processed Data/Processed Eload.rds")
@@ -16,6 +22,7 @@ additional_variable_data <-  readRDS("Processed Data/Processed Additional Variab
 
 # Run Functions ----
 
+## Simple dfs ----
 simple_df_created <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data)
 simple_df_created_hourly <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data,
                                                     convert_to_data_interval = "Hourly")
@@ -24,6 +31,27 @@ simple_df_created_daily <- nmecr::create_dataframe(eload_data = eload_data, temp
 simple_df_created_monthly <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data,
                                                      convert_to_data_interval = "Monthly")
 
+## Additional variable dfs -----
+
+added_variable_df_created <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data,
+                                             additional_independent_variables = additional_variable_data,
+                                             additional_variable_aggregation = c(median))
+
+
+added_variable_df_created_hourly <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data,
+                                                     additional_independent_variables = additional_variable_data,
+                                                     additional_variable_aggregation = c(median),
+                                                     convert_to_data_interval = "Hourly")
+
+added_variable_df_created_daily <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data,
+                                                   additional_independent_variables = additional_variable_data,
+                                                   additional_variable_aggregation = c(median),
+                                                   convert_to_data_interval = "Daily")
+
+added_variable_df_created_monthly <- nmecr::create_dataframe(eload_data = eload_data, temp_data = temp_data,
+                                                     additional_independent_variables = additional_variable_data,
+                                                     additional_variable_aggregation = c(median),
+                                                     convert_to_data_interval = "Monthly")
 
 
 # Tests ---
@@ -33,6 +61,15 @@ testthat::test_that('Simple Dataframes are created correctly', {
   expect_identical(simple_df_hourly, simple_df_created_hourly)
   expect_identical(simple_df_daily, simple_df_created_daily)
   expect_identical(simple_df_monthly, simple_df_created_monthly)
+
+})
+
+testthat::test_that('Dataframes with additional variable are created correctly', {
+
+  expect_identical(added_variable_df, added_variable_df_created)
+  expect_identical(added_variable_df_hourly, added_variable_df_created_hourly)
+  expect_identical(added_variable_df_daily, added_variable_df_created_daily)
+  expect_identical(added_variable_df_monthly, added_variable_df_created_monthly)
 
 })
 
